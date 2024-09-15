a71b1e7c5f1db8c46c31e1d6c97569ed37ce5aca
tnnHiu <trinhnguyennhathieu@gmail.com>
2024-09-16 03:48:20 +0700

    week 4

---
+++ Week_4/Week_4/Connector.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_4
+{
+    internal class Connector
+    {
+        private string? ip;
+        private int port;
+        public string Ip { get; set; }
+        public int Port { get; set; }
+
+        private static Connector? instance;
+        public static Connector? Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    Console.WriteLine("Please initialize connector first");
+                }
+                return instance;
+            }
+        }
+        public static void Initialize(string ip, int port)
+        {
+            if (instance == null)
+            {
+                instance = new Connector(ip, port);
+            }
+        }
+
+        private Connector(string ip, int port)
+        {
+            Ip = ip;
+            Port = port;
+            Console.WriteLine("Connected");
+        }
+        public override string ToString()
+        {
+            return $"ip: {Ip}, port: {Port}";
+        }
+    }
+}
---
+++ Week_4/Week_4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Week_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Connector have not initialize yet: ");
+            Connector? connector = Connector.Instance;
+            Console.WriteLine(connector);
+
+            //-------------------------------------------
+            Console.WriteLine("Initialize connector: ");
+            Connector.Initialize("192.168.1.1", 1001);
+
+            //-------------------------------------------
+            Console.WriteLine("Connector 1: ");
+            Connector? connector1 = Connector.Instance;
+            Console.WriteLine(connector1);
+
+            //-------------------------------------------
+            Console.WriteLine("Connector 2: ");
+            Connector? connector2 = Connector.Instance;          
+            Console.WriteLine(connector2);
+        }
+    }
+}
